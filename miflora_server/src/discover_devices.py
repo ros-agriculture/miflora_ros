27ebad322228c703104db4ac7949bcccc286dafd
droter <matt@droter.com>
2020-09-11 16:57:10 -0700

    initial commit

--- miflora_server/src/discover_devices.py
+++ miflora_server/src/discover_devices.py
@@ -4,7 +4,7 @@ from btlewrap import BluepyBackend
 from miflora import miflora_scanner 
 
 backend = BluepyBackend
-devices = miflora_scanner.scan(backend, 10, passive=True )
+devices = miflora_scanner.scan(backend, 10)
 print('Found {} devices:'.format(len(devices)))
 for device in devices:
     print('{}'.format(device))
